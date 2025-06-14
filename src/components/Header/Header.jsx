2128f58f42f78a958a2044865c91700803c7fcc4
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-06-13 23:51:23 -0300

    feat: Adiciona logo e estilos ao componente Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
-
 import styles from './Header.module.css';
 
 export default function Header(){
     return(
         <header className={styles.containerHeader}>
-            <Link href='/' className={styles.containerHeaderTitle}>
-                    <h1>Junior <span>Barber</span></h1>
-            </Link>
+            <div className={styles.containerHeaderBoxTitle}>
+                <Link href='/' className={styles.containerHeaderTitle}>
+                        <h1>Junior <span>Barber</span></h1>
+                </Link>
+            </div>
         </header>
     )
 }
