d32bf8a994ab1fd086973d1a060f61075d47c66b
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-06-13 23:24:44 -0300

    feat: Cria pasta de componentes e pasta com o componente Header.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+import styles from './Header.module.css';
+
+export default function Header(){
+    return(
+        <header className={styles.containerHeader}>
+            <Link href='/' className={styles.containerHeaderTitle}>
+                    <h1>Junior <span>Barber</span></h1>
+            </Link>
+        </header>
+    )
+}
