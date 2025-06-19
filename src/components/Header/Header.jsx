f5579b07b5f443d0183f5cb78f22dd1f2dbf4938
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-06-19 00:12:52 -0300

    feat: implementa header responsivo com menu hambúrguer e animação de sublinhado nos links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,15 @@
+'use client';
 import Link from "next/link";
 import styles from './Header.module.css';
 
+import { useState } from "react";
+
 export default function Header(){
+
+    const [menuAberto, setMenuAberto] = useState(false);
+
+    const alternaMenu = () => setMenuAberto(!menuAberto);
+
     return(
         <header className={styles.containerHeader}>
             <div className={styles.containerHeaderBoxTitle}>
@@ -9,6 +17,31 @@ export default function Header(){
                         <h1>Junior <span>Barber</span></h1>
                 </Link>
             </div>
+            <button className={styles.btnMenu} onClick={alternaMenu}>
+                ☰
+            </button>
+            <nav className={`${styles.navMenu} ${menuAberto ? styles.menuAberto : ''}`}>
+                <ul>
+                    <li>
+                        <Link href='/' className={styles.navLinks}>Home</Link>
+                    </li>
+                    <li>
+                        <Link href='/sobre' className={styles.navLinks}>Sobre</Link>
+                    </li>
+                    <li>
+                        <Link href='/servicos' className={styles.navLinks}>Serviços</Link>
+                    </li>
+                    <li>
+                        <a href='#galeria' className={styles.navLinks}>Galeria</a>
+                    </li>
+                    <li>
+                        <a href='#agendamento' className={styles.navLinks}>Agende um Horário</a>
+                    </li>
+                    <li>
+                        <a href='#contato' className={styles.navLinks}>Contato</a>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
